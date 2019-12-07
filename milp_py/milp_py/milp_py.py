7a7460992da03604c7cddf565317b958d2db74a0
詹 家彰 <kz85430@gmail.com>
2019-12-07 12:12:45 +0800

    簡單註解 學習表格

---
+++ milp_py/milp_py/milp_py.py
@@ -0,0 +1,62 @@
+
+
+# coding=utf-8
+# 很重要的教學網站  https://medium.com/opex-analytics/optimization-modeling-in-python-pulp-gurobi-and-cplex-83a62129807a
+
+import pandas as pd
+from pandas import DataFrame, Series
+
+# make matplotlib plots appear inside the notebook
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from pylab import rcParams
+rcParams['figure.figsize'] = 20, 10 ############################ <-Use this to change the plot
+#from IPython.core.display import HTML
+#HTML("<style>.container { width:100%; }</style>")
+
+
+'''準備資料  有四種發電來源 利用pandas做成表格  不同發電來源排碳成本的表格 '''
+energies = ["coal", "gas", "diesel", "wind"]
+df_energy = DataFrame({"co2_cost": [30, 5, 15, 0]}, index=energies)
+
+
+
+'''有很多部機組   不同的單位有不同的特性   variable_cost 不知道是什麼東西 
+ 變成表格 key 會變成row  直軸的index  每個機組的名稱 最後變成直軸是每個機組的名稱 橫軸是機組特性 (最大最小功率)
+ '''
+all_units = ["coal1", "coal2", 
+             "gas1", "gas2", "gas3", "gas4", 
+             "diesel1", "diesel2", "diesel3", "diesel4"]
+
+ucp_raw_unit_data = {
+        "energy": ["coal", "coal", "gas", "gas", "gas", "gas", "diesel", "diesel", "diesel", "diesel"],
+        "initial" : [400, 350, 205, 52, 155, 150, 78, 76, 0, 0],
+        "min_gen": [100, 140, 78, 52, 54.25, 39, 17.4, 15.2, 4, 2.4],
+        "max_gen": [425, 365, 220, 210, 165, 158, 90, 87, 20, 12],
+        "operating_max_gen": [400, 350, 205, 197, 155, 150, 78, 76, 20, 12],
+        "min_uptime": [15, 15, 6, 5, 5, 4, 3, 3, 1, 1],
+        "min_downtime":[9, 8, 7, 4, 3, 2, 2, 2, 1, 1],
+        "ramp_up":   [212, 150, 101.2, 94.8, 58, 50, 40, 60, 20, 12],
+        "ramp_down": [183, 198, 95.6, 101.7, 77.5, 60, 24, 45, 20, 12],
+        "start_cost": [5000, 4550, 1320, 1291, 1280, 1105, 560, 554, 300, 250],
+        "fixed_cost": [208.61, 117.37, 174.12, 172.75, 95.353, 144.52, 54.417, 54.551, 79.638, 16.259],
+        "variable_cost": [22.536, 31.985, 70.5, 69, 32.146, 54.84, 40.222, 40.522, 116.33, 76.642],
+        }
+df_units = DataFrame(ucp_raw_unit_data, index=all_units)
+
+
+
+print(df_units.index)
+#print(df_units['coal1'])
+
+
+
+# Add a derived co2-cost column by merging with df_energies
+# Use energy key from units and index from energy dataframe
+df_up = pd.merge(df_units, df_energy, left_on="energy", right_index=True)
+df_up.index.names=['units']
+
+# Display first rows of new 'df_up' Data Frame
+df_up.head()
+
+
